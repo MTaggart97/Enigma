d7d80cb728f4470188fae2c7fe7b9d93ea606cc2
Matthew Taggart <mtaggart123@gmail.com>
2021-03-13 21:58:16 +0000

    Fleshed out main script for manual testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,20 @@ int main(const int , const char**) {
     const std::string file{"../src/resources/plugboard1.txt"};
     PlugBoard pb{file};
 
-    char b = pb.get('a');
-    char y = pb.get('x');
-
-    std::cout << "a maps to " << b << std::endl;
-    std::cout << "x maps to " << y << std::endl;
+    std::string text;
+    for(;;) {
+        std::cout << "Enter the text you want to encrypt\n";
+        std::cout << "Press '\\' and Enter to exit" << std::endl;
+        std::getline(std::cin, text);
+        std::cout << "Encrypted message is\n";
+        for (size_t i = 0; i < text.size(); i++) {
+            if (text.at(i) == '\\') {
+                std::cout << std::endl;
+                std::cout << "'\\' detected, exiting..." << std::endl;
+                return 0;
+            }
+            std::cout << pb.get(text.at(i));
+        }
+        std::cout << std::endl;
+    }
 }
