1c5e8364ccb5709c1c9f25de610a52a613a19db5
Matthew Taggart <mtaggart123@gmail.com>
2021-06-15 19:59:21 +0100

    Added comments for Enigma Machine

--- src/Enigma/headers/EnigmaMachine.h
+++ src/Enigma/headers/EnigmaMachine.h
@@ -13,14 +13,44 @@ using Enigma::Reflector;
 namespace Enigma {
     class EnigmaMachine {
     private:
+        /**
+         * Pointer to plugboard.
+         */
         PlugBoard* plugboard;
+        /**
+         * Pointer to first rotor.
+         */
         Rotor* rotor1;
+        /**
+         * Pointer to second rotor.
+         */
         Rotor* rotor2;
+        /**
+         * Pointer to third rotor.
+         */
         Rotor* rotor3;
+        /**
+         * Pointer to reflector.
+         */
         Reflector* reflector;
     public:
+        /**
+         * Initialise the Enigma Machine.
+         *
+         * @param   config  Machine config
+         */
         EnigmaMachine(const MACHINE_CONFIG*);
+        /**
+         * Destroys the pointers to each of the 
+         * machines components.
+         */
         ~EnigmaMachine();
+        /**
+         * Encrypts a given charater.
+         *
+         * @param   char    Character to encrypt
+         * @return          The encrypted character
+         */
         char encrypt(const char);
     };
 }
